809c7250fab57e48115e8575e3f37305da12520b
ycchu8992 <ycchu8992@gmail.com>
2023-06-11 17:37:34 +0800

    Modify error of macro

--- hw1/test_result/output_1.cpp
+++ hw1/test_result/output_1.cpp
@@ -2,7 +2,6 @@
 12:#pragma token on										   |	6:#pragma token on
 13:int main(const int argc, const char ** argv) {						   |	7:int main(const int argc, const char ** argv) {
 14:  /* Hope the stack can survive this call (finger-crossed x) */				   |	8:  /* Hope the stack can survive this call (finger-crossed x) */
-#macro:__LINE__											   |	#id:__LINE__
 15:  const int ret = printf("Call factorial(%d) \100 line (%d)\n", factorial(atoi(argv[1])), __L   |	9:  const int ret = printf("Call factorial(%d) \100 line (%d)\n", factorial(atoi(argv[1])), __LI
 16:  return ret;										   |	10:  return ret;
 17:};												   |	11:};
